ef02fbb11cf695c6148c6c7f6a153bcd6ad7df12
Shivansh Jaiswal <shivansh12kol34@gmail.com>
2026-03-29 16:38:53 +0530

    add: cuda based code to find collision

---
+++ assn2/sha256/bruteforce.cu
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "sha256.cuh"
+
+#define DIGEST_LENGTH 32
+#define PREFIX_LEN 7
+
+
+// The first few characters are blank space (basically first 3)
+// since we have only 33 million threads here, thus the ids are 
+// restricted to 33 million initially, but combining with the offset
+// we eventually reach longer (non space-filled) payloads
+__device__ void gen_str(uint64_t id, unsigned char* prefix){
+    for(int i = PREFIX_LEN - 1; i >= 0; i--){
+        prefix[i] = (char) ((id%95) + 32);
+        id /= 95;
+    }
+}
+
+// Kernel specifically for calculation of Target Hash
+__global__ void gen_target(
+    const unsigned char* data, 
+    int len, 
+    unsigned char* result
+){
+    SHA256_CTX ctx;
+    sha256_init(&ctx);                  // initialize hashing state
+    sha256_update(&ctx, data, len);     
+    sha256_final(&ctx, result);
+}
+
+
+__global__ void main_kernel(
+    unsigned char* target_hash,
+    int num_bytes_to_match,
+    unsigned char* suffix,
+    int suffix_len,
+    volatile int* found_flag, 
+    unsigned char* found,
+    uint64_t offset                     // offset = Multiple of threads per batch
+) {
+    uint64_t thread_id = offset + (uint64_t) blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (*found_flag == 1) return;
+
+    unsigned char payload[64];
+    unsigned char candidate_hash[DIGEST_LENGTH];
+
+    gen_str(thread_id, payload);
+
+    for(int i = 0; i < suffix_len; i++){ 
+        payload[PREFIX_LEN + i] = suffix[i];
+    }
+
+    SHA256_CTX ctx; 
+    sha256_init(&ctx);
+    sha256_update(&ctx, payload, PREFIX_LEN + suffix_len);
+    sha256_final(&ctx, candidate_hash);
+
+    // to save extra computation
+    bool match = true; 
+    for(int i = 0; i< num_bytes_to_match; i++){
+        if(candidate_hash[i] != target_hash[i]) {
+            match = false;
+            break;
+        }
+    }
+
+
+    if (match) {
+        if(atomicExch((int*) found_flag, 1) == 0) {
+            for(int i = 0; i < PREFIX_LEN + suffix_len; i++){
+                found[i] = payload[i]; 
+            }
+            found[PREFIX_LEN + suffix_len] = '\0';
+        }
+    }
+}
+
+
+
+int main() {
+    const char *target = "shivanshj24@iitk.ac.in";
+    int target_len = strlen(target);
+
+    const char *suffix = "@iitk.ac.in";
+    int suffix_len = strlen(suffix);
+
+    int num_bits_to_match = 48; 
+    int num_bytes_to_match = num_bits_to_match/8; 
+
+    int* h_found_flag = 0; 
+    unsigned char h_found[64] = {0};
+    unsigned char h_target_hash[DIGEST_LENGTH] = {0};
+
+    unsigned char   *d_target,
+                    *d_target_hash, 
+                    *d_suffix,
+                    *d_found;
+    
+    int *d_found_flag;
+
+    cudaMalloc((void**)&d_target, target_len);
+    cudaMalloc((void**)&d_target_hash, DIGEST_LENGTH);
+    cudaMalloc((void**)&d_suffix, suffix_len);
+    cudaMalloc((void**)&d_found_flag, sizeof(int));
+    cudaMalloc((void**)&h_found, 64);
+       
+    // Compute the target hash using specific kernel
+    cudaMemcpy(d_target, target, target_len, cudaMemcpyHostToDevice);
+
+    gen_target<<<1,1>>>(d_target, target_len, d_target_hash);
+    cudaDeviceSynchronize();
+    cudaFree(d_target);
+
+    cudaMemcpy(d_suffix, suffix, suffix_len, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_found_flag, h_found_flag, sizeof(int), cudaMemcpyHostToDevice);
+
+    int threadPerBlock = 512; 
+    int numBlocks = 65535;
+
+    uint64_t threads_per_batch = (uint64_t) threadPerBlock * numBlocks; 
+    uint64_t global_offset = 0;
+
+    printf("Launching GPU. Target: %d. Number of Threads Launched: %llu", num_bits_to_match, threads_per_batch);
+
+    // Now just keep launching the kernel till found_flag is 1. 
+
+    while(!h_found_flag) {
+        main_kernel<<<numBlocks, threadPerBlock>>>(
+            d_target_hash,
+            num_bytes_to_match,
+            d_suffix, 
+            suffix_len,
+            d_found_flag,
+            d_found, 
+            global_offset
+        );
+
+        cudaDeviceSynchronize();
+        cudaMemcpy(&h_found_flag, d_found_flag, sizeof(int), cudaMemcpyDeviceToHost);
+
+        global_offset += threads_per_batch;
+        printf("\rHashes Computed: %llu ", global_offset);
+        fflush(stdout);
+    }
+
+
+    if(h_found_flag) {
+        cudaMemcpy(h_found, d_found, 64, cudaMemcpyDeviceToHost);
+        printf("\n\n Found Collision: %s", h_found);
+    }
+
+    cudaFree(d_target_hash);
+    cudaFree(d_suffix);
+    cudaFree(d_found_flag);
+    cudaFree(d_found);
+}
--- assn2/sha256/main.c
+++ assn2/sha256/main.c
@@ -7,7 +7,10 @@
 #include <pthread.h>
 
 #define DIGEST_LENGTH 32
+
 #define NUM_THREADS 16
+#define NUM_REPS 100
+#define NUM_BITS_TO_MATCH 32
 
 unsigned char target_hash[DIGEST_LENGTH];
 size_t num_bytes_to_match;
@@ -15,18 +18,17 @@ size_t num_bytes_to_match;
 const char *suffix = "@iitk.ac.in";
 size_t suffix_len;
 
+int iter = 0;
 volatile int found_flag = 0;
-
-const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const size_t charset_size = 62;
-const int prefix_len = 6;
+const int prefix_len = 7;
 
 uint64_t thread_iters[NUM_THREADS] = {0};
+uint64_t rep_iters[NUM_REPS] = {0};
 
 void* worker(void *args){
     int thread_id = *(int*) args;
 
-    unsigned int seed = time(NULL) ^ (thread_id * 1999);
+    unsigned int seed = time(NULL) ^ (thread_id * 1999) ^ (iter * 6666);
     unsigned char *to_bake = (unsigned char *) malloc(prefix_len + suffix_len + 1);
 
     memcpy(to_bake + prefix_len, suffix, suffix_len);
@@ -36,8 +38,9 @@ void* worker(void *args){
     uint64_t iterations = 0;
 
     while(!found_flag){
+
         for(int i = 0; i < prefix_len; i++){
-            to_bake[i] = charset[rand_r(&seed) % charset_size];
+            to_bake[i] = (char) ((rand_r(&seed) % 95) + 32);
         }
 
         SHA256((unsigned char*) to_bake, prefix_len + suffix_len, candidate_hash);
@@ -46,7 +49,7 @@ void* worker(void *args){
         if(memcmp(candidate_hash, target_hash, num_bytes_to_match) == 0) {
             if(!found_flag) {
                 found_flag = 1;
-                printf("Found a collision!\n\nTarget Hash: ");
+                printf("\nTarget Hash: ");
 
                 for(int l = 0; l < DIGEST_LENGTH; l++){
                     printf("%02x", *(target_hash + l));
@@ -72,18 +75,7 @@ void* worker(void *args){
     return NULL;
 }
 
-
-int main(){
-    const char *data = "shivanshj24@iitk.ac.in";
-    SHA256((unsigned char*) data, strlen(data), target_hash);
-
-    suffix_len = strlen(suffix);
-
-    // If we wish to compare the first 24 bits, then we should ideally be 
-    // looping through more numbers.
-    size_t num_bits_to_match = 24;
-    num_bytes_to_match = num_bits_to_match/8;
-
+void solve(int target){
     pthread_t threads[NUM_THREADS];
     int threads_ids[NUM_THREADS];
 
@@ -101,6 +93,35 @@ int main(){
         tot += thread_iters[i];
     }
 
-    printf("Search complete. Total iterations: %lld \n", tot);
+    printf("Search Iteration %d complete. Total iterations: %lld \n", iter, tot);
+    rep_iters[iter] = tot;
+    iter++;
+    found_flag = 0;
+
+    if(target > iter){
+        solve(target);
+    }
+}
+
+
+int main(){
+
+    const char *data = "shivanshj24@iitk.ac.in";
+    SHA256((unsigned char*) data, strlen(data), target_hash);
+    suffix_len = strlen(suffix);
+
+    num_bytes_to_match = NUM_BITS_TO_MATCH/8;
+
+    solve(NUM_REPS);
+
+    uint64_t sum = 0;
+    for(int i = 0; i < NUM_REPS; i++){
+        sum += rep_iters[i];
+        printf("%d, ", rep_iters[i]);
+    }
+    double average = sum/NUM_REPS;
+
+    printf("\n\n\nAverage Iterations Taken: %f\n", average);
     return 0;
 }
+
